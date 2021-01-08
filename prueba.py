d998c9e6f40278eb14ae36cfacbd5d72c7b3b93f
ctj01 <flexdancer507@gmail.com>
2021-01-08 17:35:33 -0500

    first commit

---
+++ prueba.py
@@ -0,0 +1,32 @@
+"""from concurrent.futures import ThreadPoolExecutor
+
+def f(n):
+    return n  # No need to wrap in list
+
+master_list = []
+with ThreadPoolExecutor(max_workers=20) as exec:
+    for result in exec.map(f, range(1, 100)):
+        master_list.append(result)
+
+print(master_list)
+"""
+class MyClass:
+    def __init__(self, name, age):
+      self.name = name
+      self.age = age
+    def d ():
+        print("hola")
+
+        """[objecto]
+          """
+    @age.setter
+    def age (self, value):
+        self.__age = value
+
+p = MyClass
+p.d()
+print(p)
+
+x = MyClass("no", 6)
+
+print(x)
