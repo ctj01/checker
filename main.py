d315a3e6e9a30535c54041bff7be730779a8ce5a
ctj01 <flexdancer507@gmail.com>
2021-02-02 06:15:18 -0500

    adding static file

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from proxy_checker import  ProxyChecker
 from concurrent import futures
 import requests
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"), name="static",)
+templates = Jinja2Templates(directory="templates")
+app.mount("/", StaticFiles(directory="templates", html = True), name="site")
+
+
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:8000",
@@ -23,9 +32,9 @@ def check_proxy_(proxy):
     if x != False:
         return proxy
 
-##@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request : Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/{proxies}")
 async  def getproxy_(proxies : str):
